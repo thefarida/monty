095c77e9a3b800eb80c18b452e7201ba641698d3
thefarida <adamuibrahimfarida@gmail.com>
2022-10-07 22:09:12 +0100

    Push and Pall

--- push_and_pall.c
+++ push_and_pall.c
@@ -21,7 +21,7 @@ void pall(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * push - Add a new value to the list
+ * push - Adds a new value to the list
  * @stack: Double linked list
  * @line_number: File line for execution
  */
