969d4ef6c0545bb37c47f47d52a6be62a9d5684c
thefarida <adamuibrahimfarida@gmail.com>
2022-10-07 05:07:34 +0100

    Nop opcode

--- monty.h
+++ monty.h
@@ -64,7 +64,7 @@ void free_stack(stack_t *head);
 void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
 void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
+void f_nop(stack_t **stack, unsigned int counter);
 void f_sub(stack_t **head, unsigned int counter);
 void f_div(stack_t **head, unsigned int counter);
 void f_mul(stack_t **head, unsigned int counter);
---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_nop - This does nothing
+ * @stack: Nothing
+ * @counter: Nothing
+ */
+
+void f_nop(stack_t **stack, unsigned int counter)
+{
+	(void) stack;
+	(void) counter;
+}
